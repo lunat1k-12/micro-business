253b496a6c3ff7d72334e482aeb8cddb604db329
Yvgen Chemeris <chemeris.lunat1k@gmail.com>
2020-05-17 14:17:47 +0300

    initial business added

---
+++ business/src/main/java/com/micro/microbusiness/MicroBusinessApplication.java
@@ -0,0 +1,13 @@
+package com.micro.microbusiness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroBusinessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroBusinessApplication.class, args);
+    }
+
+}
---
+++ business/src/test/java/com/micro/microbusiness/MicroBusinessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micro.microbusiness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class MicroBusinessApplicationTests {
+
+    //@Test
+    void contextLoads() {
+    }
+
+}

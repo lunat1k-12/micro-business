1b566ec66aa0cda204a8127d4521ebaccc9395ac
Yvgen Chemeris <chemeris.lunat1k@gmail.com>
2020-05-27 15:08:06 +0300

    client service added

---
+++ client-service/src/main/java/com/micro/client/service/entity/Client.java
@@ -0,0 +1,36 @@
+package com.micro.client.service.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Table(name = "client")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "business_area_id")
+    private Long businessAreaId;
+}
---
+++ client-service/src/test/java/com/micro/client/service/ClientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micro.client.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class ClientServiceApplicationTests {
+
+//    @Test
+    void contextLoads() {
+    }
+
+}
